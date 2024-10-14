9bedf86bd213c61962cb66cf55e7d8ee8758f405
Your Name <you@example.com>
2024-10-14 16:47:20 +0000

    m

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Coding challenge.
+'''
+
+
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result '''
+    count = 0
+    while(n > 1):
+        if (n % 3 == 0):
+            n //=3
+            count+=3
+        elif (n % 2 == 0):
+            n //= 2
+            count+=2
+        else:
+            count+=n
+            n = 1
+            
+    return count
