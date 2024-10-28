e0832ecdeef5b0ed792d9959aeafe11042d3f0eb
Your Name <you@example.com>
2024-10-28 14:13:26 +0000

    moaz

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" 0-UTF-8 Validation """
+
+
+def validUTF8(data):
+    """ Return: True if data is a valid UTF-8
+    encoding, else return False"""
+    x = 0
+    while(x < len(data)):
+        byte = data[x]
+
+        num_bytes = 0
+        if (byte & 0b10000000) == 0:
+            num_bytes = 1
+        elif (byte & 0b11000000) == 0b11000000:
+            num_bytes = 2
+        elif (byte & 0b11100000) == 0b11100000:
+            num_bytes = 3
+        elif (byte & 0b11110000) == 0b11110000:
+            num_bytes = 4
+        else:
+            return False
+
+        if x + num_bytes > len(data):
+            return False
+
+        for j in range(1,num_bytes):
+            if (data[x + j] & 0b11000000) != 0b10000000:
+                return False
+        x+=num_bytes
+    return True
