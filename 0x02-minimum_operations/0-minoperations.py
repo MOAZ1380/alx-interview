807a2c114633e97874763bbf797c9c551183009a
Your Name <you@example.com>
2024-10-31 19:38:03 +0000

    s

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,31 +2,28 @@
 """UTF-8 Validation"""
 
 
-def get_leading_set_bits(num):
-    """returns the number of leading set bits (1)"""
-    set_bits = 0
-    helper = 1 << 7
-    while helper & num:
-        set_bits += 1
-        helper = helper >> 1
-    return set_bits
-
-
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding"""
-    bits_count = 0
-    for i in range(len(data)):
-        if bits_count == 0:
-            bits_count = get_leading_set_bits(data[i])
-            '''1-byte (format: 0xxxxxxx)'''
-            if bits_count == 0:
-                continue
-            '''a character in UTF-8 can be 1 to 4 bytes long'''
-            if bits_count == 1 or bits_count > 4:
-                return False
+
+    while(x < len(data)):
+        byte = data[x]
+        num_bytes = 0
+
+        if (byte & 0b10000000) == 0:
+            num_bytes = 1
+        elif (byte & 0b11000000) == 0b11000000:
+            num_bytes = 2
+        elif (byte & 0b11100000) == 0b11100000:
+            num_bytes = 3
+        elif (byte & 0b11110000) == 0b11110000:
+            num_bytes = 4
         else:
-            '''checks if current byte has format 10xxxxxx'''
-            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+            return False
+
+        if x + num_bytes > len(data):
+            return False
+
+        for j in range(1, num_bytes):
+            if (data[x + j] & 0b11000000) != 0b10000000:
                 return False
-        bits_count -= 1
-    return bits_count == 0
+        x += num_bytes
+    return True
